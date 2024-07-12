59a2956c0c9c0cbb8d87e4d9d914db7ad958796e
AOvsyannikov <aovsyannikov@it-one.ru>
2024-07-12 22:52:42 +0300

    updated all

--- src/test/java/tests/SearchTest.java
+++ src/test/java/tests/SearchTest.java
@@ -4,10 +4,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 import static io.appium.java_client.AppiumBy.accessibilityId;
 import static io.appium.java_client.AppiumBy.id;
 import static io.qameta.allure.Allure.step;
+import static
 
 @DisplayName("Тесты для Google Pixel 6")
 @Tag("mob")
@@ -35,5 +37,6 @@ public class SearchTest extends TestBase{
         });
         step("Verify content found", () ->
                 $(id("org.wikipedia.alpha:id/page_list_item_title")).click());
+                $(id("org.wikipedia.alpha:id/view_wiki_error_text")).shouldHave(text("Error"));
     }
 }
