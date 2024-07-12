864ac01b53638a6fc190074564d1b0ec92459211
AOvsyannikov <aovsyannikov@it-one.ru>
2024-07-12 22:54:14 +0300

    updated SearchTest.java

--- src/test/java/tests/SearchTest.java
+++ src/test/java/tests/SearchTest.java
@@ -9,7 +9,6 @@ import static com.codeborne.selenide.Selenide.*;
 import static io.appium.java_client.AppiumBy.accessibilityId;
 import static io.appium.java_client.AppiumBy.id;
 import static io.qameta.allure.Allure.step;
-import static
 
 @DisplayName("Тесты для Google Pixel 6")
 @Tag("mob")
